3a1b004f6ab6b9125e818415bc823323192a69a3
이윤구 <lee36656@gmail.com>
2019-07-22 17:07:48 +0900

    Updates

---
+++ precache-manifest.39d54dae9b4cf1b8b03c4f7a8c43d49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f216b07b38ef5e29d320f91e952511",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "053589f94fdf44001039",
+    "url": "/portal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portal/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "053589f94fdf44001039",
+    "url": "/portal/static/js/main.96f1fe89.chunk.js"
+  },
+  {
+    "revision": "05d4c05c31e6e52e5e67",
+    "url": "/portal/static/js/runtime~main.3c49a560.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
